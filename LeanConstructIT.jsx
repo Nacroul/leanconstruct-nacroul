57075d14b7f562824b7c94495c57ea2e369d53c4
Nacroul <51131420+Nacroul@users.noreply.github.com>
2025-07-20 19:49:50 +0200

    Add files via upload

---
+++ LeanConstructIT.jsx
@@ -0,0 +1,4 @@
+// Codul React complet cu formular, logo și traduceri va fi inserat aici manual după test
+export default function LeanConstructIT() {
+  return <div>Lean Construct IT</div>;
+}
---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import LeanConstructIT from './LeanConstructIT'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LeanConstructIT />
+  </React.StrictMode>
+)
